14528ed18557bc3d85ea67f8953ee441037596a6
rdemello <demez884@hotmail.com>
2019-11-24 19:17:35 +0000

    Fixed timestep issue

--- climateSensitivityModel.m
+++ climateSensitivityModel.m
@@ -1,36 +1,48 @@
 clc;
+  
+%% Define Variables
 
-% Read in data from a NetCDF file (name.nc)
-  forcingFile='inputForcing.nc';
-  %display the variables and metadata stored in the NetCDF file
-  ncdisp(forcingFile); 
-  %read in the variable called Aerosol_cloud_lifetime
-  aclf = ncread(forcingFile,'Aerosol_cloud_lifetime');
-  Year = ncread(forcingFile,'Year');
-% Note that the Skeie et al. Radiative Forcing is with respect to 1750, but we start in 1850.
+forcingFile='inputForcing.nc';
+%ncdisp(forcingFile); %display all variables for the forcing file
+  
+% set variables for all forcings
+F_aerosolCloudLife = ncread(forcingFile,'Aerosol_cloud_lifetime');
+F_ghg = ncread(forcingFile,'Greenhouse_gases');
+F_stratOzone = ncread(forcingFile,'Strato_ozone');
+F_tropOzone = ncread(forcingFile,'Tropo_ozone');
+F_stratoWater = ncread(forcingFile,'Strato_water');
+F_aerosolDirect = ncread(forcingFile,'Aerosol_direct');
+F_aerosolCloudAlbedo = ncread(forcingFile,'Aerosol_cloud_albedo');
+F_landUse = ncread(forcingFile,'Land_use');
+F_snowAlbedo = ncread(forcingFile,'Snow_albedo');
+F_solar = ncread(forcingFile,'Solar');
+F_volcanic = ncread(forcingFile,'Volcanic');
+Year = ncread(forcingFile,'Year');
 
-%% Declare All Variables
+F_total = zeros(num_years,1);
 
-F_total = load('RCP8.5_Forcing.txt');
+F_RCPForcing = load('RCP8.5_Forcing.txt');
 
-ECS = 1;            %climate sensitivity 
+F_selected = F_RCPForcing;
+
+ECS = 1.5;          %climate sensitivity 
 a = 3.74/ECS;       %defined alpha for climate feedback parameter
 
 density = 1027;     %"p" density of water in kg/m3
 c_p = 4218;         %specific heat capacity of water J/kg/K
 
-k = 1;              %vertical diffusivity cm^2/s
-h_u = 100;          %upper height m
-h_d = 900;          %lower height m
+k = 0.0001;         %vertical diffusivity m^2/s
+h_u = 100;         	%upper height m
+h_d = 900;         	%lower height m
 
 C_u = density*c_p*h_u; %thermal interia upper J/(m^2 K^1 s^1/2)
 C_d = density*c_p*h_d; %thermal interia deep J/(m^2 K^1 s^1/2)
 
-g = (2*k*c_p*density)/(h_u+h_d); %heat diffusion
+g = (2*k*c_p*density)/(h_u+h_d); %heat diffusion m2 * 1/s * J * 1/kg * 1/K * kg * 1/m3 * 1/m = J/m^2 * K * s
 
 num_years = 161;
-timestep = 1;
-F_diff = 1;         %variable from code snippet
+secInYear = 60*60*24*365;
+timestep =  secInYear;
 
 T_d = zeros(num_years,1); %empty array for deep temp
 T_u = zeros(num_years,1); %empty array for upper temp
@@ -38,22 +50,23 @@ T_u = zeros(num_years,1); %empty array for upper temp
 %% Run Loop
 
 for i = 1:num_years-1;
-    T_u(i+1,1) = (F_total(i,2) - (a*(T_u(i,1))) - (g*(T_u(i,1) - T_d(i,1))))/C_u; 
-    T_d(i+1,1) = (g*(T_u(i,1) - T_d(i,1)))/C_d;
+    upper_energy = timestep * (F_selected(i,2) - (a*(T_u(i))) - (g*(T_u(i) - T_d(i))));  %solved from equation
+    T_u(i+1) = T_u(i) + upper_energy/C_u; 
+    
+    deep_energy = timestep * g*(T_u(i) - T_d(i));
+    T_d(i+1) = T_d(i) + deep_energy/C_d;
 end
-  
+
 %% Plot Graph
 
-  figure(1);
-  plot(Year,T_u,'r','LineWidth',3);
-  title('Global Mean Temperatures','FontWeight','bold','FontSize',14);
-  ylabel('Temp Variation from Total Forcing on Upper Ocean','FontSize',12);
-  xlabel('Year','FontWeight','bold','FontSize',12);
-  hold all
-  plot(Year,T_d,'r','LineWidth',3);
-  title('Global Mean Temperatures','FontWeight','bold','FontSize',14);
-  ylabel('Temp Variation from Total Forcing on Lower Ocean','FontSize',12);
-  xlabel('Year','FontWeight','bold','FontSize',12);
+figure(1);
+plot(Year,T_u,'c','LineWidth',2);
+title('Temperature Variation Based on RCP8.5 Forcing','FontWeight','bold','FontSize',14);
+ylabel('Temperature Variation','FontSize',12);
+xlabel('Year','FontWeight','bold','FontSize',12);
+hold all
+plot(Year,T_d,'b','LineWidth',2);
+legend('Upper Ocean','Deep Ocean');
 
 %% Old Equations
 
@@ -82,7 +95,6 @@ end
 %   T_median = HadCRUT4_all{1,1}(1:161,1);
 %   T_95percent = HadCRUT4_all{1,2}(1:161,1);
 %   T_5percent = HadCRUT4_all{1,3}(1:161,1);
-
 
 % a plot of your results compared to data
 %   figure(1);
